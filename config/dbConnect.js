aa613fcf8a30ee6e578d8bd68ce30df08476738f
gunesh <gunesh26@gmail.com>
2023-10-17 22:52:13 +0530

    1 file modified

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,14 +2,6 @@ import mongoose from 'mongoose';
 const USER = 'usergunesh';
 const PASSWORD = 'usergun';
 const DB = 'zee5';
-// mongoose.connect(
-//   `mongodb+srv://${USER}:${PASSWORD}@cluster0.znykm.mongodb.net/${DB}?retryWrites=true&w=majority`,
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     serverApi: "1"
-//   }
-// );
 
 const connectDB = async () => {
   try {
