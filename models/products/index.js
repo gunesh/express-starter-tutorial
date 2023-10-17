f9ba186b8c4cda7c8cae886a691f6354db099614
Gunesh Asatkar <gunesh.asatkar@kantar.com>
2023-10-17 22:22:51 +0530

    Starter2

--- models/products/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const productsSchema = require("./products.schema");
-
-const products = mongoose.model("products", productsSchema);
-
-module.exports = products;
--- models/user/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const userSchema = require("./user-schema");
-
-const user = mongoose.model("users", userSchema);
-
-module.exports = user;
